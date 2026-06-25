2485e4f5b4bf354edce092ed44264c29c9bcf19e
kstudilova <kstudilova@gmail.com>
2026-06-25 12:36:55 +0300

    read command line arguments

--- studilova.ksenia/U1/args.cpp
+++ studilova.ksenia/U1/args.cpp
@@ -34,4 +34,64 @@ namespace
 }
 
 studilova::Args studilova::parseArgs(int argc, char *argv[])
-{}
+{
+  Args args{ "", "", false, false, true };
+
+  if ((argc - 1) > 2)
+  {
+    args.valid_ = false;
+    return args;
+  }
+
+  const std::string inputPrefix = "in:";
+  const std::string outputPrefix = "out:";
+
+  for (int i = 1; i < argc; ++i)
+  {
+    const std::string argument = argv[i];
+
+    if (hasPrefix(argument, inputPrefix))
+    {
+      if (args.hasInput_)
+      {
+        args.valid_ = false;
+        return args;
+      }
+
+      args.inputFile_ = removePrefix(argument, inputPrefix);
+
+      if (args.inputFile_.empty())
+      {
+        args.valid_ = false;
+        return args;
+      }
+
+      args.hasInput_ = true;
+    }
+    else if (hasPrefix(argument, outputPrefix))
+    {
+      if (args.hasOutput_)
+      {
+        args.valid_ = false;
+        return args;
+      }
+
+      args.outputFile_ = removePrefix(argument, outputPrefix);
+
+      if (args.outputFile_.empty())
+      {
+        args.valid_ = false;
+        return args;
+      }
+
+      args.hasOutput_ = true;
+    }
+    else
+    {
+      args.valid_ = false;
+      return args;
+    }
+  }
+
+  return args;
+}
