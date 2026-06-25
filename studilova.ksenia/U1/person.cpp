b7112679e3005768cb745022aec3d2f15481cc8c
kstudilova <kstudilova@gmail.com>
2026-06-25 11:36:24 +0300

    add help functions to append person

--- studilova.ksenia/U1/person.cpp
+++ studilova.ksenia/U1/person.cpp
@@ -1,5 +1,37 @@
 #include "person.hpp"
 
+namespace
+{
+  size_t getNextCapacity(size_t capacity)
+  {
+    if (capacity == 0)
+    {
+      return 4;
+    }
+    return capacity * 2;
+  }
+
+  void reservePersonArray(studilova::PersonArray &array, size_t newCapacity)
+  {
+    if (newCapacity <= array.capacity_)
+    {
+      return;
+    }
+
+    studilova::Person* newData = new studilova::Person[ newCapacity ];
+
+    for (size_t i = 0; i < array.size_; ++i)
+    {
+      newData[i] = array.data_[i];
+    }
+
+    delete[] array.data_;
+
+    array.data_ = newData;
+    array.capacity_ = newCapacity;
+  }
+}
+
 void studilova::initPersonArray(PersonArray &array)
 {
   array.data_ = nullptr;
