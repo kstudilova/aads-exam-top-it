6b2dcd7c1f09a1e0a26a4523ed362a495821118f
kstudilova <kstudilova@gmail.com>
2026-06-25 13:13:24 +0300

    fix output

--- studilova.ksenia/U1/main.cpp
+++ studilova.ksenia/U1/main.cpp
@@ -8,75 +8,90 @@
 
 int main(int argc, char *argv[])
 {
+  studilova::PersonArray persons;
+  studilova::initPersonArray(persons);
+
+  if (argc > 3)
+  {
+    std::cerr << "Invalid arguments\n";
+    studilova::destroyPersonArray(persons);
+    return 0;
+  }
+
   const studilova::Args args = studilova::parseArgs(argc, argv);
 
   if (!args.valid_)
   {
+    std::cerr << "Invalid arguments\n";
+    studilova::destroyPersonArray(persons);
     return 1;
   }
 
-  studilova::PersonArray persons;
-  studilova::initPersonArray(persons);
+  std::ifstream inputFile;
+  std::istream* input = &std::cin;
 
-  try
+  if (args.hasInput_)
   {
-    std::ifstream inputFile;
-    std::istream* input = &std::cin;
+    inputFile.open(args.inputFile_);
 
-    if (args.hasInput_)
+    if (!inputFile)
     {
-      inputFile.open(args.inputFile_);
-
-      if (!inputFile)
-      {
-        studilova::destroyPersonArray(persons);
-        return 2;
-      }
-
-      input = &inputFile;
+      std::cerr << "Cannot open file\n";
+      studilova::destroyPersonArray(persons);
+      return 2;
     }
-
-    std::string line;
-
-    while (std::getline(*input, line))
-    {
-      studilova::Person person;
-
-      if (studilova::parsePersonLine(line, person) && !studilova::containsPersonId(persons, person.id_))
-      {
-        studilova::appendPerson(persons, person);
-      }
-    }
-
-    inputFile.close();
-
-    std::ofstream outputFile;
-    std::ostream* output = &std::cout;
-
-    if (args.hasOutput_)
-    {
-      outputFile.open(args.outputFile_);
-
-      if (!outputFile)
-      {
-        studilova::destroyPersonArray(persons);
-        return 2;
-      }
-      output = &outputFile;
-    }
-
-    for (size_t i = 0; i < persons.size_; ++i)
-    {
-      *output << persons.data_[i].id_ << ' ' << persons.data_[i].info_ << '\n';
-    }
-
-    studilova::destroyPersonArray(persons);
+    input = &inputFile;
   }
-  catch (...)
+
+  size_t count = 0;
+  size_t ignore = 0;
+  std::string line;
+
+  while (std::getline(*input, line))
   {
-    studilova::destroyPersonArray(persons);
-    return 2;
+    studilova::Person person;
+
+    if (studilova::parsePersonLine(line, person) && !studilova::containsPersonId(persons, person.id_))
+    {
+      studilova::appendPerson(persons, person);
+      ++count;
+    }
+    else
+    {
+      ++ignore;
+    }
   }
+  inputFile.close();
+
+  std::ofstream outputFile;
+  std::ostream* output = &std::cout;
+
+  if (args.hasOutput_)
+  {
+    outputFile.open(args.outputFile_);
+
+    if (!outputFile)
+    {
+      std::cerr << "Cannot open file\n";
+      studilova::destroyPersonArray(persons);
+      return 2;
+    }
+    output = &outputFile;
+  }
+
+  for (size_t i = 0; i < persons.size_; ++i)
+  {
+    *output << persons.data_[i].id_ << ' ' << persons.data_[i].info_ << '\n';
+  }
+
+  if (persons.size_ == 0)
+  {
+    *output << '\n';
+  }
+
+  std::cerr << count << ' ' << ignore << '\n';
+
+  studilova::destroyPersonArray(persons);
 
   return 0;
 }
